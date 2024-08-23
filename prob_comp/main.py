f89bc8811381cc98319e021fd6a3b8bdb250e97b
stellamaranhao <109309376+stellamaranhao@users.noreply.github.com>
2024-08-22 23:05:51 -0300

    commit um

--- prob_comp/main.py
+++ prob_comp/main.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
-# Filtrar os dados
 dados = pd.read_csv('product_data.csv')
 df_product = dados.filter(items=['Product type', 'Price'])
 
-# Função para calcular as estatísticas
 def cent_stats(foo):
     avg = foo.mean()
     median = foo.median()
@@ -30,21 +28,17 @@ def cent_stats(foo):
         'curtose': curtose
     }
 
-# Separando os dados por tipo de produto
 df_hairstyle = df_product[df_product['Product type'] == 'haircare'].copy()
 df_skin_care = df_product[df_product['Product type'] == 'skincare'].copy()
 df_cosmetics = df_product[df_product['Product type'] == 'cosmetics'].copy()
 
-# Aplicando a função e adicionando as novas colunas
 for df in [df_hairstyle, df_skin_care, df_cosmetics]:
     stats = cent_stats(df['Price'])
     for stat_name, stat_value in stats.items():
         df[stat_name] = stat_value
 
-# Exibindo os DataFrames resultantes (opcional)
 print(df_hairstyle.head())
 print(df_skin_care.head())
 print(df_cosmetics.head())
 
-# Retornando os DataFrames modificados
 df_hairstyle, df_skin_care, df_cosmetics
